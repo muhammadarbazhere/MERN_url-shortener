edb34a76778cde2ca0c571e9f5d35a53a036369b
Muhammad Arbaz <muhammad.arbazhere@gmail.com>
2025-08-06 15:27:59 +0500

    commit

---
+++ backend/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const urlRoutes = require("./routes/urlRoutes");
+
+const app = express();
+const port = process.env.PORT || 3000 ;
+
+const corsOptions = {
+   origin: [
+    process.env.FRONTEND_BASE_URL,    // ✅ Production frontend from .env
+    "http://localhost:5173"           // ✅ Local frontend
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions)); 
+
+app.use(express.json());
+
+app.use("/url", urlRoutes);
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connection successful");
+  } catch (error) {
+    console.log("Connection error", error);
+  }
+}
+connectDB();
+
+// ✅ Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal Server Error" });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
